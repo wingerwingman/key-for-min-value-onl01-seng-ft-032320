52a4102bcaa87f510a13d4604090fde18c3836db
Justin Winger <justinwinger4@gmail.com>
2020-03-26 17:00:36 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,11 +3,12 @@
 
 def key_for_min_value(name_hash)
   lowest_num = 0
-  lowerst_key = nil
+  lowest_key = nil
   name_hash.each do |name, value|
     if lowest_num == 0 || value < lowest_num
       lowest_num = value
       lowest_key = name
     end
   end
+  lowest_key
 end
