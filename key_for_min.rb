ea1443a7690c9863fbbd0237343cca271b785b02
Justin Winger <justinwinger4@gmail.com>
2020-03-26 16:59:20 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,13 +2,12 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  num = 0
-  name_hash.map do |name, value|
-  num = value
-  if num > value
-    num = value
-  else
-    name
+  lowest_num = 0
+  lowerst_key = nil
+  name_hash.each do |name, value|
+    if lowest_num == 0 || value < lowest_num
+      lowest_num = value
+      lowest_key = name
+    end
   end
-end
 end
